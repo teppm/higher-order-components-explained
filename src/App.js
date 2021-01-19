4e9c7821f20f28d1c9da9eba59839749dbd66727
Madis Tepp <54039418+teppm@users.noreply.github.com>
2021-01-19 21:36:43 +0100

    fixed bnode error

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import React from 'react';
 import UserProfile from './components/user-profile/user-profile.component';
 import UserList from './components/user-list/user-list.component';
 
-import './App.scss';
+import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <UserList dataSource='https://jsonplaceholder.typicode.com/users' />
+    <div className="App">
+      <UserList dataSource="https://jsonplaceholder.typicode.com/users" />
       <UserProfile
-        name='Yihua'
-        email='yihuazhang@gmail.com'
-        dataSource='https://jsonplaceholder.typicode.com/posts'
+        name="Yihua"
+        email="yihuazhang@gmail.com"
+        dataSource="https://jsonplaceholder.typicode.com/posts"
       />
     </div>
   );
